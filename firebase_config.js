8b6712cd70049d246369b8da90372d22dca54d2e
sukang <sukang@MacBookPro.lan>
2024-11-07 12:21:48 +0900

    Add firebase config module

---
+++ firebase_config.js
@@ -0,0 +1,12 @@
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAcNxdfupglLDv1Z18wYRBn-AZelw8OhEk",
+    authDomain: "introduce-ourselves.firebaseapp.com",
+    projectId: "introduce-ourselves",
+    storageBucket: "introduce-ourselves.appspot.com",
+    messagingSenderId: "510644864006",
+    appId: "1:510644864006:web:b252705d880832136c6f87",
+    measurementId: "G-SK0R8C7M9R"
+};
+
+export { firebaseConfig };
